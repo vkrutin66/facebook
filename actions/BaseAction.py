02d2383cf735607129f44dcd606b39d7ff6f4af5
Vladyslav Krutin <vkrutin66@gmail.com>
2019-03-27 14:48:46 +0200

    run.sh

--- actions/BaseAction.py
+++ actions/BaseAction.py
@@ -11,11 +11,13 @@ class BaseAction:
     def __init__(self):
         options = webdriver.ChromeOptions()
         home = str(Path.home())
-        options.add_argument("user-data-dir=" + home + "/AppData/Local/Google/Chrome/User Data")
+
         if platform.system() == 'Windows':
+            options.add_argument("user-data-dir=" + home + "/AppData/Local/Google/Chrome/User Data")
             Popen('taskkill /F /IM chrome.exe', shell=True)
             self.driver = webdriver.Chrome(executable_path="drivers/chromedriver.exe", chrome_options=options)
         else:
+            options.add_argument("user-data-dir=~/Library/Application Support/Google/Chrome")
             os.system("killall -9 'Google Chrome'")
             self.driver = webdriver.Chrome(executable_path="drivers/chromedriver_mac", chrome_options=options)
         print("Open browser Chrome")
